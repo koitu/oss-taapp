8ad882756e42d34ccbd687c742eae07eda4c941e
ander9991 <sg8002@nyu.edu>
2025-10-24 10:59:07 -0400

    feat: Add abstract chat client API for multi-platform chat services

---
+++ src/chat_client_api/src/chat_client_api/__init__.py
@@ -0,0 +1,26 @@
+"""Chat Client API - Abstract contract for chat service implementations.
+
+This module provides abstract base classes and factory functions that define
+the contract for chat client implementations (Discord, Slack, etc.).
+
+Public API:
+    - Client: Abstract base class for chat clients
+    - ChatMessage: Abstract base class for chat messages
+    - Channel: Abstract base class for channels
+    - get_client: Factory function to get a client implementation
+    - get_message: Factory function to create message instances
+    - get_channel: Factory function to create channel instances
+
+"""
+
+from chat_client_api.client import Client, get_client
+from chat_client_api.message import Channel, ChatMessage, get_channel, get_message
+
+__all__ = [
+    "Channel",
+    "ChatMessage",
+    "Client",
+    "get_channel",
+    "get_client",
+    "get_message",
+]
