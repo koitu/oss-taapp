7c9ef5d6bb74b4a08c3d49231ca4444c3cf79a8a
Alper Mumcular <alper.mumcular@ug.bilkent.edu.tr>
2025-11-08 00:48:39 -0500

    Update __init__.py

--- src/clients/discord_client_service_client/__init__.py
+++ src/clients/discord_client_service_client/__init__.py
@@ -1,3 +1,4 @@
 """A client library for accessing Discord Client Service"""
 
-from .client import AuthenticatedClient, Client
+from .client import AuthenticatedClient as AuthenticatedClient
+from .client import Client as Client
