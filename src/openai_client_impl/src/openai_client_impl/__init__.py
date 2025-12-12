1b0dd4aaae9a0147e59ccf169abea25f4d9ef9af
ander9991 <sg8002@nyu.edu>
2025-12-12 18:04:46 -0500

    lint fix

--- src/openai_client_impl/src/openai_client_impl/__init__.py
+++ src/openai_client_impl/src/openai_client_impl/__init__.py
@@ -1,7 +1,6 @@
 """Public API for the openai_client_impl package."""
 
-import ai_api
-
+import ai_api as ai_api
 from openai_client_impl.ai_client import AIClientImpl as AIClientImpl
 from openai_client_impl.errors import MissingOpenAIKeyError as MissingOpenAIKeyError
 from openai_client_impl.storage import init_db as init_db
